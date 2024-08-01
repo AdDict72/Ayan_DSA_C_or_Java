05ef289ace6c16a9a09778af725bb0624194b51a
AdDict72 <ayandasf1s@gmail.com>
2024-08-02 00:34:38 +0530

    array mid

---
+++ DSA Course Practice/Array/Medium/SortAnArrayOfZeroOneTwo.java
@@ -0,0 +1,61 @@
+// Problem Statement: Given an array consisting of only 0s, 1s, and 2s. 
+// Write a program to in-place sort the array without using inbuilt sort functions.
+// ( Expected: Single pass-O(N) and constant space)
+
+// This problem is a variation of the popular Dutch National flag algorithm. 
+
+import java.util.Scanner;
+
+public class SortAnArrayOfZeroOneTwo {
+
+    public static void  swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    public static void sortedArray(int n, int[] arr){
+        int low = 0, mid = 0, high = n - 1;
+
+        while (mid <= high) {
+            switch (arr[mid]) {
+                case 0:
+                    swap(arr, low, mid);
+                    low++;
+                    mid++;
+                    break;
+                case 1:
+                    mid++;
+                    break;
+                case 2:
+                    swap(arr, mid, high);
+                    high--;
+                    break;
+            }
+        }
+    System.out.print("Sorted array: ");
+    for(int i = 0 ; i<n ; i++){
+      System.err.print(arr[i]+" ");
+    }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of terms in array : ");
+        int n = sc.nextInt();
+        
+        int arr[] =  new int[n];
+        System.out.println("Enter array elements one by one(0 or 1 or 2)...");
+        for(int i = 0 ; i<n ; i++){
+            int element;
+            while (true) {
+                element = sc.nextInt();
+                if (element == 0 || element == 1 || element == 2) {
+                    break;
+                } else {
+                    System.out.println("Invalid input. Please enter 0, 1, or 2:");
+                }
+            }
+            arr[i] = element;
+        }
+        sortedArray(n, arr);
+    }
+}
---
+++ DSA Course Practice/Array/Medium/TwoSum_Problem.java
@@ -0,0 +1,36 @@
+// Problem Statement: Given an array of integers arr[] and an integer target.
+// 1st variant: Return YES if there exist two numbers such that their sum is equal to the target. Otherwise, return NO.
+// 2nd variant: Return indices of the two numbers such that their sum is equal to the target. Otherwise, we will return {-1, -1}.
+// Note: You are not allowed to use the same element twice. Example: If the target is equal to 6 and num[1] = 3, then nums[1] + nums[1] = target is not a solution.
+
+import java.util.Scanner;
+class TwoSum_Problem {
+
+    public static String twoSum(int n, int arr[], int target){
+        for(int i = 0; i<n ; i++){
+            for(int j = i+1 ; j<n ; j++){
+               if (arr[i]+arr[j]==target) {
+                return "YES";
+               }
+            }
+        }
+        return "NO";
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of terms in array : ");
+        int n = sc.nextInt();
+        
+        int arr[] =  new int[n];
+        System.out.println("Enter array elements one by one...");
+        for(int i = 0 ; i<n ; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Enter the target : ");
+        int target = sc.nextInt();
+
+        String a = twoSum(n, arr, target);
+        System.out.println("Answer for variant 1: " + a);
+    }
+}
